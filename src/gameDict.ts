47591d2a82240329e1c6fd4abefd7e30f1f50fd1
Oliver Terrell <93229853+oliverterrell@users.noreply.github.com>
2024-12-30 16:10:52 -0500

    new levels

--- src/gameDict.ts
+++ src/gameDict.ts
@@ -99,59 +99,59 @@ export const skull_2 = {
   word: 'skull',
   board: {
     r1: {
-      c3: 'bush',
-      c6: 'rock',
-      c8: 'rock',
-      c9: 'chest'
+      c4: 'bush',
+      c7: 'rock',
+      c9: 'rock',
+      c10: 'chest'
     },
     r2: {
-      c2: 'bush',
-      c3: 'chest',
-      c6: 'rock',
-      c8: 'rock',
+      c3: 'bush',
+      c4: 'chest',
+      c7: 'rock',
       c9: 'rock',
-      c11: 'drumstick'
+      c10: 'rock',
+      c12: 'drumstick'
     },
     r3: {
-      c3: 'bush',
-      c6: 'rock',
-      c7: 'rum',
-      c8: 'rock',
-      c9: 'chest'
+      c4: 'bush',
+      c7: 'rock',
+      c8: 'rum',
+      c9: 'rock',
+      c10: 'chest'
     },
     r4: {
-      c2: 'bush',
-      c3: 'chest',
-      c6: 'rock',
-      c8: 'rock',
+      c3: 'bush',
+      c4: 'chest',
+      c7: 'rock',
       c9: 'rock',
-      c11: 'drumstick'
+      c10: 'rock',
+      c12: 'drumstick'
     },
     r5: {
-      c3: 'bush',
-      c6: 'rock',
-      c8: 'rock',
-      c9: 'chest'
+      c4: 'bush',
+      c7: 'rock',
+      c9: 'rock',
+      c10: 'chest'
     },
     r6: {
-      c2: 'bush',
-      c3: 'chest',
-      c6: 'rock',
-      c7: 'rum',
-      c8: 'rock',
+      c3: 'bush',
+      c4: 'chest',
+      c7: 'rock',
+      c8: 'rum',
       c9: 'rock',
-      c11: 'drumstick'
+      c10: 'rock',
+      c12: 'drumstick'
     },
     r7: {
-      c3: 'bush',
-      c6: 'rock',
-      c8: 'rock',
-      c9: 'chest'
+      c4: 'bush',
+      c7: 'rock',
+      c9: 'rock',
+      c10: 'chest'
     },
     r8: {
-      c6: 'rock',
-      c8: 'rock',
-      c9: 'rock'
+      c7: 'rock',
+      c9: 'rock',
+      c10: 'rock'
     }
   }
 }
@@ -176,6 +176,7 @@ export const matey_3 = {
     },
     r4: {
       c4: 'bush',
+      c7: 'rum',
       c10: 'bush'
     },
     r5: {
@@ -230,24 +231,554 @@ export const sword_4 = {
       c11: 'chest'
     },
     r5: {
+      c2: 'drumstick',
+      c5: 'water',
+      c6: 'water',
+      c7: 'water',
+      c8: 'water',
+      c9: 'water',
+      c12: 'drumstick'
+    },
+    r6: {
       c5: 'rock',
+      c8: 'rock',
+    },
+    r7: {
+      c1: 'water',
+      c2: 'water',
+      c3: 'water',
+      c4: 'water',
+      c5: 'water',
+      c6: 'drumstick',
+      c7: 'drumstick',
+      c8: 'water',
+      c9: 'water',
+      c10: 'water',
+      c11: 'water',
+      c12: 'water',
+      c13: 'water',
+    },
+    r8: {
+      c1: 'chest',
+      c3: 'chest',
+      c5: 'chest',
+      c8: 'chest',
+      c11: 'chest',
+      c13: 'chest'
+    }
+  }
+}
+export const jolly_5 = {
+  dayNum: 5,
+  word: 'jolly',
+  board: {
+    r1: {
+      c5: 'rock',
+      c6: 'rock',
+      c7: 'rock',
+      c8: 'rock',
+      c9: 'rock',
+    },
+    r2: {
+      c4: 'rock',
+      c10: 'rock'
+    },
+    r3: {
+      c3: 'rock',
+      c5: 'chest',
+      c6: 'chest',
+      c8: 'chest',
+      c9: 'chest',
+      c11: 'rock'
+    },
+    r4: {
+      c3: 'rock',
+      c5: 'chest',
+      c6: 'chest',
+      c8: 'chest',
+      c9: 'chest',
+      c11: 'rock'
+    },
+    r5: {
+      c4: 'rock',
+      c7: 'chest',
+      c10: 'rock'
+    },
+    r6: {
+      c4: 'rock',
+      c7: 'chest',
+      c10: 'rock'
+    },
+    r7: {
+      c5: 'rock',
+      c7: 'rock',
+      c9: 'rock',
+    },
+    r8: {
+      c6: 'rock',
       c8: 'rock',
     }
   }
 }
-
-export const jolly_5 = {
-  dayNum: 5,
-  word: 'jolly',
-  board: defaultBoard
-}
 export const roger_6 = {
   dayNum: 6,
   word: 'roger',
-  board: defaultBoard
+  board: {
+    r1: {
+      c6: 'rum',
+      c7: 'chest'
+    },
+    r2: {
+      c1: 'water',
+      c2: 'water',
+      c3: 'water',
+      c4: 'water',
+      c5: 'water',
+      c6: 'water',
+      c7: 'water',
+      c8: 'water',
+      c9: 'water',
+      c10: 'water',
+      c11: 'rum',
+      c12: 'chest',
+      c13: 'chest',
+    },
+    r3: {
+      c6: 'rum',
+      c7: 'chest'
+    },
+    r4: {
+      c1: 'chest',
+      c2: 'chest',
+      c3: 'rum',
+      c4: 'water',
+      c5: 'water',
+      c6: 'water',
+      c7: 'water',
+      c8: 'water',
+      c9: 'water',
+      c10: 'water',
+      c11: 'water',
+      c12: 'water',
+      c13: 'water',
+    },
+    r5: {
+      c6: 'rum',
+      c7: 'chest'
+    },
+    r6: {
+      c1: 'water',
+      c2: 'water',
+      c3: 'water',
+      c4: 'water',
+      c5: 'water',
+      c6: 'water',
+      c7: 'water',
+      c8: 'water',
+      c9: 'water',
+      c10: 'water',
+      c11: 'rum',
+      c12: 'chest',
+      c13: 'chest',
+    },
+    r7: {
+      c6: 'rum',
+      c7: 'chest'
+    },
+    r8: {
+      c1: 'chest',
+      c2: 'chest',
+      c3: 'rum',
+      c4: 'water',
+      c5: 'water',
+      c6: 'water',
+      c7: 'water',
+      c8: 'water',
+      c9: 'water',
+      c10: 'water',
+      c11: 'water',
+      c12: 'water',
+      c13: 'water',
+    }
+  }
 }
 export const booty_7 = {
   dayNum: 7,
   word: 'booty',
-  board: defaultBoard
+  board: {
+    r1: {
+      c4: 'bush',
+      c10: 'bush'
+    },
+    r2: {
+      c3: 'bush',
+      c5: 'bush',
+      c7: 'chest',
+      c9: 'bush',
+      c11: 'bush',
+    },
+    r3: {
+      c2: 'bush',
+      c3: 'chest',
+      c5: 'chest',
+      c6: 'bush',
+      c8: 'bush',
+      c9: 'chest',
+      c11: 'chest',
+      c12: 'bush',
+    },
+    r4: {
+      c1: 'bush',
+      c4: 'rum',
+      c7: 'bush',
+      c10: 'rum',
+      c13: 'bush',
+    },
+    r5: {
+      c2: 'bush',
+      c3: 'chest',
+      c5: 'chest',
+      c6: 'bush',
+      c8: 'bush',
+      c9: 'chest',
+      c11: 'chest',
+      c12: 'bush',
+    },
+    r6: {
+      c3: 'bush',
+      c5: 'bush',
+      c7: 'chest',
+      c9: 'bush',
+      c11: 'bush',
+    },
+    r7: {
+      c4: 'bush',
+      c10: 'bush'
+    },
+  }
+}
+export const plank_8 = {
+  dayNum: 8,
+  word: 'plank',
+  board: {
+    r1: {
+      c3: 'water',
+      c4: 'water',
+      c5: 'water',
+    },
+    r2: {
+      c3: 'water',
+      c4: 'water',
+      c5: 'water',
+      c8: 'chest',
+      c9: 'drumstick',
+      c10: 'drumstick',
+      c11: 'drumstick',
+      c12: 'chest'
+    },
+    r3: {
+      c3: 'water',
+      c4: 'water',
+      c5: 'water',
+      c8: 'chest',
+      c9: 'water',
+      c10: 'water',
+      c11: 'water',
+      c12: 'chest'
+    },
+    r4: {
+      c3: 'water',
+      c4: 'water',
+      c5: 'water',
+      c8: 'chest',
+      c9: 'water',
+      c10: 'water',
+      c11: 'water',
+      c12: 'chest'
+    },
+    r5: {
+      c3: 'bush',
+      c4: 'bush',
+      c5: 'bush',
+      c8: 'chest',
+      c9: 'water',
+      c10: 'water',
+      c11: 'water',
+      c12: 'chest'
+    },
+    r6: {
+      c3: 'water',
+      c4: 'water',
+      c5: 'water',
+      c8: 'chest',
+      c9: 'drumstick',
+      c10: 'drumstick',
+      c11: 'drumstick',
+      c12: 'chest'
+    },
+    r7: {
+      c3: 'water',
+      c4: 'water',
+      c5: 'water',
+    },
+    r8: {
+      c3: 'water',
+      c4: 'water',
+      c5: 'water',
+    }
+  }
+}
+export const aargh_9 = {
+  dayNum: 9,
+  word: 'aargh',
+  board: {
+    r1: {
+      c5: 'bush',
+    },
+    r2: {
+      c3: 'chest',
+      c4: 'bush',
+      c5: 'drumstick',
+      c6: 'bush',
+      c7: 'chest',
+      c9: 'drumstick',
+      c10: 'bush',
+    },
+    r3: {
+      c3: 'bush',
+      c7: 'bush',
+      c9: 'bush',
+      c11: 'bush',
+      c12: 'chest',
+    },
+    r4: {
+      c2: 'bush',
+      c5: 'chest',
+      c8: 'bush',
+      c10: 'chest',
+      c11: 'rum',
+      c12: 'bush',
+    },
+    r5: {
+      c3: 'bush',
+      c7: 'bush',
+      c9: 'bush',
+      c11: 'bush',
+      c12: 'chest',
+    },
+    r6: {
+      c3: 'chest',
+      c4: 'bush',
+      c5: 'drumstick',
+      c6: 'bush',
+      c7: 'chest',
+      c9: 'drumstick',
+      c10: 'bush',
+    },
+    r7: {
+      c5: 'bush',
+    },
+  }
+}
+export const skull_10 = {
+  dayNum: 10,
+  word: 'skull',
+  board: {
+    r1: {
+      c4: 'bush',
+      c7: 'rock',
+      c9: 'rock',
+      c10: 'chest'
+    },
+    r2: {
+      c3: 'bush',
+      c4: 'chest',
+      c7: 'rock',
+      c9: 'rock',
+      c10: 'rock',
+      c12: 'drumstick'
+    },
+    r3: {
+      c4: 'bush',
+      c7: 'rock',
+      c8: 'rum',
+      c9: 'rock',
+      c10: 'chest'
+    },
+    r4: {
+      c3: 'bush',
+      c4: 'chest',
+      c7: 'rock',
+      c9: 'rock',
+      c10: 'rock',
+      c12: 'drumstick'
+    },
+    r5: {
+      c4: 'bush',
+      c7: 'rock',
+      c9: 'rock',
+      c10: 'chest'
+    },
+    r6: {
+      c3: 'bush',
+      c4: 'chest',
+      c7: 'rock',
+      c8: 'rum',
+      c9: 'rock',
+      c10: 'rock',
+      c12: 'drumstick'
+    },
+    r7: {
+      c4: 'bush',
+      c7: 'rock',
+      c9: 'rock',
+      c10: 'chest'
+    },
+    r8: {
+      c7: 'rock',
+      c9: 'rock',
+      c10: 'rock'
+    }
+  }
+}
+export const chest_11 = {
+  dayNum: 11,
+  word: 'chest',
+  board: {
+    r1: {
+      c5: 'chest',
+      c9: 'chest',
+      c13: 'chest',
+    },
+    r2: {
+      c2: 'water',
+      c3: 'water',
+      c4: 'water',
+      c6: 'water',
+      c7: 'water',
+      c8: 'water',
+      c10: 'water',
+      c11: 'water',
+      c12: 'water',
+    },
+    r3: {
+      c2: 'water',
+      c3: 'water',
+      c4: 'water',
+      c5: 'drumstick',
+      c6: 'water',
+      c7: 'water',
+      c8: 'water',
+      c9: 'drumstick',
+      c10: 'water',
+      c11: 'water',
+      c12: 'water',
+      c13: 'drumstick',
+    },
+    r4: {
+      c2: 'water',
+      c3: 'water',
+      c4: 'water',
+      c6: 'water',
+      c7: 'water',
+      c8: 'water',
+      c10: 'water',
+      c11: 'water',
+      c12: 'water',
+    },
+    r5: {
+      c1: 'chest',
+      c3: 'drumstick',
+      c5: 'chest',
+      c7: 'drumstick',
+      c9: 'chest',
+      c11: 'drumstick',
+      c13: 'chest',
+    },
+    r6: {
+      c2: 'water',
+      c3: 'water',
+      c4: 'water',
+      c6: 'water',
+      c7: 'water',
+      c8: 'water',
+      c10: 'water',
+      c11: 'water',
+      c12: 'water',
+    },
+    r7: {
+      c2: 'water',
+      c3: 'water',
+      c4: 'water',
+      c5: 'drumstick',
+      c6: 'water',
+      c7: 'water',
+      c8: 'water',
+      c9: 'drumstick',
+      c10: 'water',
+      c11: 'water',
+      c12: 'water',
+      c13: 'drumstick',
+    },
+    r8: {
+      c2: 'water',
+      c3: 'water',
+      c4: 'water',
+      c5: 'chest',
+      c6: 'water',
+      c7: 'water',
+      c8: 'water',
+      c9: 'chest',
+      c10: 'water',
+      c11: 'water',
+      c12: 'water',
+      c13: 'chest',
+    }
+  }
+}
+export const matey_12 = {
+  dayNum: 12,
+  word: 'matey',
+  board: {
+    r1: {
+      c6: 'bush',
+      c7: 'bush',
+      c8: 'bush'
+    },
+    r2: {
+      c5: 'bush',
+      c9: 'bush'
+    },
+    r3: {
+      c4: 'bush',
+      c6: 'chest',
+      c8: 'chest',
+      c10: 'bush'
+    },
+    r4: {
+      c4: 'bush',
+      c7: 'rum',
+      c10: 'bush'
+    },
+    r5: {
+      c4: 'bush',
+      c6: 'chest',
+      c7: 'chest',
+      c8: 'chest',
+      c10: 'bush'
+    },
+    r6: {
+      c4: 'chest',
+      c5: 'bush',
+      c9: 'bush',
+      c10: 'chest'
+    },
+    r7: {
+      c5: 'chest',
+      c6: 'bush',
+      c7: 'bush',
+      c8: 'bush',
+      c9: 'chest',
+    }
+  }
 }
